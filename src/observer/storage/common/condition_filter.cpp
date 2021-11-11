3eb4d04e0069b6229760cb09f751b0a8b0d889a5
zyf2000 <zyf2000ivy@gmail.com>
2021-11-11 17:09:25 +0800

    fix: null in select

--- src/observer/storage/common/condition_filter.cpp
+++ src/observer/storage/common/condition_filter.cpp
@@ -128,7 +128,7 @@ RC DefaultConditionFilter::init(Table &table, const Condition &condition)
     //  }
     // NOTE：这里没有实现不同类型的数据比较，比如整数跟浮点数之间的对比
     // 但是选手们还是要实现。这个功能在预选赛中会出现
-    if (type_left != type_right)
+    if (type_left != type_right && type_left != NULLS && type_right != NULLS)
     {
         return RC::SCHEMA_FIELD_TYPE_MISMATCH;
     }
